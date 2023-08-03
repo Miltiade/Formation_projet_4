f6d1fa51a1e4546b0468c48894d40747f84c39e1
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-08-03 20:26:50 +0300

    finished & tested script "book.py"

--- book.py
+++ book.py
@@ -3,55 +3,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-
 class Book:
     """Instancie la classe Livre."""
 
-    def __init__(self, url, product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url):
+    def __init__(self, url):
         """Initialise l'état de la classe : son existence, et les caractéristiques à scraper."""
         self.url = url
-        self.product_page_url = product_page_url
-        self.universal_product_code : universal_product_code
-        self.title = title
-        self.price_including_tax = price_including_tax
-        self.price_excluding_tax = price_excluding_tax
-        self.number_available = number_available
-        self.product_description = product_description,
-        self.category = category
-        self.review_rating = review_rating
-        self.image_url = image_url
+        self.product_page_url = None
+        self.universal_product_code = None
+        self.title = None
+        self.price_including_tax = None
+        self.price_excluding_tax = None
+        self.number_available = None
+        self.product_description = None
+        self.category = None
+        self.review_rating = None
+        self.image_url = None
         
-    def scrape(url):
+    def scrape(self):
         """Scrape les données du livre"""
-        response = requests.get(url)
+        response = requests.get(self.url)
         book = BeautifulSoup(response.content, "html.parser")
-        universal_product_code = (
+        self.universal_product_code = (
             book.find("th", string="UPC").find_parent().find("td").text.strip()
         )
         div = book.find("div", class_="product_main")
-        title = div.find("h1").text
+        self.title = div.find("h1").text
         table = book.find("table", class_="table table-striped")
         table_td = table.find_all("td")
-        price_including_tax = table_td[3].text
-        price_excluding_tax = table_td[2].text
-        number_available = table_td[5].text
-        product_description = book.find_all("p")[3].text
-        category = book.find_all("a")[3].text
-        review_rating = div.find("p", class_="star-rating").attrs["class"][1]
-        image_url = book.find("img").attrs["src"]
-        return [
-            url,
-            universal_product_code,
-            title,
-            price_including_tax,
-            price_excluding_tax,
-            number_available,
-            product_description,
-            category,
-            review_rating,
-            image_url,
-        ]
-
-book = Book()
-"""TEST. Crée un objet book dans la classe Book. ATTENTION : c'est dans Category que seront créés automatiquement les objets book"""
+        self.price_including_tax = table_td[3].text
+        self.price_excluding_tax = table_td[2].text
+        self.number_available = table_td[5].text
+        self.product_description = book.find_all("p")[3].text
+        self.category = book.find_all("a")[3].text
+        self.review_rating = div.find("p", class_="star-rating").attrs["class"][1]
+        self.image_url = book.find("img").attrs["src"]
