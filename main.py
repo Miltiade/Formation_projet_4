f3c9ac85f1bf8e6fba68158f03b4225b379c8707
Miltiade <smith@tutanota.de>
2023-10-28 07:13:21 +0300

    minor corrections

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from controlers.tournament_controler import TournamentControler
 tournamentControler = TournamentControler()
 tournamentControler.create_new_tournament()
 print("TOURNAMENT CREATED!")
-tournamentControler.print_player() # la fonction print_player ne devrait-elle pas s'appeler print_players? Car elle print tous les players.
+tournamentControler.print_player()
 print("PLAYERS' LIST PRINTED!")
-tournamentControler.run_first_round() # pourquoi l'utilisateur n'est-il invité à renseigner les résultats que pour 2 matches?
-                                        # alors que pour les rounds suivants, c'est 4 matches?
+tournamentControler.run_first_round()
 print("FIRST ROUND RUN SUCCESSFULLY!")
 tournamentControler.run_subsequent_rounds()
 tournamentControler.display_final_ranking()
