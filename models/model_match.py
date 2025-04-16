bb9f0f63299e8ccca8780a186ce645b39e3a4dce
Miltiade <vero@tutamail.com>
2025-04-16 09:36:49 +0300

    reviewed comments

--- models/model_match.py
+++ models/model_match.py
@@ -6,6 +6,10 @@ class Match:
         self.score_player2 = score_player2
 
     def serialize(self):
+        """
+        Serializes the match to a dictionary.
+        :return: Dictionary containing match data.
+        """
         return {
             'player1': self.player1.elo,
             'player2': self.player2.elo,
@@ -26,9 +30,12 @@ class Match:
         player1 = players.get(data['player1'])  # Use the ELO to get the Player object
         player2 = players.get(data['player2'])  # Use the ELO to get the Player object
 
+        # Check if players exist
+        # If players are not found, raise an error
         if not player1 or not player2:
             raise ValueError(f"Player data not found for ELOs: {data['player1']}, {data['player2']}")
 
+        # Create and return the Match object
         return Match(
             player1=player1,
             player2=player2,
